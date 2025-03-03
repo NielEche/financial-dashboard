3fd9bc7fd1a09d3b1fce7bd0eb18d2e197350a9d
Niel Eche <danieleche7@gmail.com>
2025-03-03 13:55:24 +0100

    had an error with duplicate call of pages on app

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ import './style/general.css'
 import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
 
-// Lazy load the Dashboard and Settings pages
-const Dashboard = lazy(() => import("./pages/Dashboard"));
-const Settings = lazy(() => import("./pages/Settings"));
 
 const App = () => {
   return (
