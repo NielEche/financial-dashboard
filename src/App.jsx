22d90b3de1d97832fdf8dea5378abaf8a33d71cd
Niel Eche <danieleche7@gmail.com>
2025-03-03 14:16:34 +0100

    settings debug ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import './style/general.css'
-import Dashboard from "./pages/Dashboard";
-import Settings from "./pages/Settings";
 
+// Lazy load the Dashboard and Settings pages
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Settings = lazy(() => import("./pages/Settings"));
 
 const App = () => {
   return (
@@ -14,12 +15,12 @@ const App = () => {
         <Sidebar />
         <div className="lg:flex-grow lg:bg-gray-100 flex-1 lg:ml-64">
           <Navbar />
-         
+          <Suspense fallback={<div>Loading...</div>}>
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/Settings" element={<Settings />} />
+              <Route path="/settings" element={<Settings />} />
             </Routes>
-         
+          </Suspense>
         </div>
       </div>
     </Router>
